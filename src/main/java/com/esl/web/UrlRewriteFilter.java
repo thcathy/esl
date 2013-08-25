a39d794a89cc9bb97354b967bb73918767855f98
thcathy <thcathy@gmail.com>
2013-08-25 21:44:40 +0800

    Fix js and context path issue

---
+++ src/main/java/com/esl/web/UrlRewriteFilter.java
@@ -0,0 +1,31 @@
+package com.esl.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class UrlRewriteFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        //
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        
+        req.getRequestDispatcher(request.getRequestURI().replace("/ESL", "")).forward(req, res);        
+    }
+
+    @Override
+    public void destroy() {
+        //
+    }
+}
