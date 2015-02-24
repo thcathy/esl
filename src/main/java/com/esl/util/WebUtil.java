ce21f8bc8f81cd1a45ded0121fe640ce84287e8f
thcathy <thcathy@gmail.com>
2015-02-24 23:11:00 +0800

    Update bing parser

--- src/main/java/com/esl/util/WebUtil.java
+++ src/main/java/com/esl/util/WebUtil.java
@@ -6,13 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Iterator;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 public class WebUtil {
 	private static Logger logger = LoggerFactory.getLogger("ESL");
@@ -34,7 +35,7 @@ public class WebUtil {
 	}
 
 	public static String[] getThumbnailsFromBing(String query) {
-		String address = "https://api.datamarket.azure.com/Data.ashx/Bing/Search/Image?Adult=%27Strict%27&ImageFilters=%27Style%3aGraphics%27&Query=%27";
+		String address = "https://api.datamarket.azure.com/Bing/Search/v1/Image?Adult=%27Strict%27&ImageFilters=%27Style%3AGraphics%27&Query=%27";
 		address += query + "%27&$top=" + MAX_QUERY_RESULT;
 		byte[] encodedPassword = ("duhLqrHx48aVaR4J/uWDXYMEtKJUbImkuuLnG1XJ170=" + ":" + "duhLqrHx48aVaR4J/uWDXYMEtKJUbImkuuLnG1XJ170=").getBytes();
 
@@ -43,28 +44,21 @@ public class WebUtil {
 			URL url = new URL (address);
 			connection = (HttpURLConnection)url.openConnection();
 			connection.setRequestMethod ("GET");
-			connection.setRequestProperty ("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Trident/5.0)");
+			//connection.setRequestProperty ("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Trident/5.0)");
 			connection.setRequestProperty("Authorization", "Basic " + new String((new org.apache.commons.codec.binary.Base64()).encodeBase64(encodedPassword)));
-
-			NodeList nodeList = getNodesFromInputStreamByTagName(connection.getInputStream(), "d:Thumbnail");
-			String[] thumbnailPaths = new String[nodeList.getLength()];
-			for (int i=0; i<nodeList.getLength(); i++) {
-				thumbnailPaths[i] = nodeList.item(i).getFirstChild().toString();
+			
+			String[] thumbnailPaths = new String[MAX_QUERY_RESULT];
+			Document doc = Jsoup.parse(connection.getInputStream(),null,"",Parser.xmlParser());
+			int i=0;
+			for (Iterator<Element> iter = doc.select("d|Thumbnail d|mediaurl").iterator(); iter.hasNext(); ) {				
+				Element e = iter.next();
+				thumbnailPaths[i++]=e.text();
 			}
+			
 			return thumbnailPaths;
 		} catch (IOException e) {
 			logger.debug("Exception during getThumbnailsFromBing from " + address, e);
 			return new String[]{};
-		}
-	}
-
-	public static NodeList getNodesFromInputStreamByTagName(InputStream in, String tagName) {
-		try {
-			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
-			return doc.getDocumentElement().getElementsByTagName(tagName);
-		} catch (Exception e) {
-			logger.debug("Exception during getNodesFromInputStreamByTagName", e);
-			return null;
 		}
 	}
 }
