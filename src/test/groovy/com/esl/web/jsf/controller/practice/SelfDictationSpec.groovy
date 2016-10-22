0a3194b2d7d440ece3eefeeb5cb8ac0f19e9e0fe
Timmy Wong <thcathy@gmail.com>
2016-10-22 08:12:19 +0800

    Speak the word once responsive voice ready

---
+++ src/test/groovy/com/esl/web/jsf/controller/practice/SelfDictationSpec.groovy
@@ -0,0 +1,28 @@
+package com.esl.web.jsf.controller.practice
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Ignore
+import spock.lang.Specification
+
+import javax.faces.component.UIViewRoot
+import javax.faces.context.FacesContext
+
+@ContextConfiguration(locations = "/com/esl/ESL-context.xml")
+@SpringBootTest
+class SelfDictationSpec extends Specification {
+    @Autowired
+    SelfDictationController selfDictationController
+
+    @Ignore
+    def "Test spring setup"() {
+        when:
+        FacesContext.getCurrentInstance().setViewRoot(new UIViewRoot(""))
+        selfDictationController.inputVocab = ["test"]
+        selfDictationController.start()
+
+        then:
+        selfDictationController.practice.answers.size()==1
+    }
+}
