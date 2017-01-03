4da3a2ce87278f46da02262d4db49b9c837fb75c
Timmy Wong <thcathy@gmail.com>
2017-01-03 22:17:47 +0800

    prime faces vocabulary practice

---
+++ src/main/webapp/js/vocabimage.js
@@ -0,0 +1,23 @@
+var picPaths = '';
+var imageCount = 0;
+
+function showImage() {
+    console.log('showImage');
+    picPaths = jQuery('#hiddenPicPaths').text().split('^_^');
+    if (picPaths.length < 1) {
+        jQuery('#gImageContent').html('#{generalMsg.imageNA}');
+    } else {
+        jQuery('#vocabImage').attr("src", picPaths[imageCount]);
+
+        if (picPaths.length == 1) {jQuery('#nextImage').hide();}
+        else {jQuery('#nextImage').show();}
+    }
+}
+
+function changeImg() {
+    imageCount = imageCount + 1;
+    if (imageCount >= picPaths.length) imageCount = 0;
+
+    jQuery('#vocabImage').attr("src", picPaths[imageCount]);
+    jQuery('#inputForm\\:answer').focus();
+}
