43f935fd9aaa291f8e7ed947368462bac2dd557b
Timmy Wong <thcathy@gmail.com>
2017-01-11 00:10:01 +0000

    DictationDaoTest.java edited online with Bitbucket

--- src/test/java/com/esl/test/dao/DictationDaoTest.java
+++ src/test/java/com/esl/test/dao/DictationDaoTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.esl.dao.dictation.IDictationDAO;
 import com.esl.entity.dictation.Dictation;
