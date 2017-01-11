c72b664bda2600c3c55276349b08e77d84e42a2a
Timmy Wong <thcathy@gmail.com>
2017-01-11 00:05:38 +0000

    GradeDaoTest.java edited online with Bitbucket

--- src/test/java/com/esl/test/dao/GradeDaoTest.java
+++ src/test/java/com/esl/test/dao/GradeDaoTest.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:/com/esl/ESL-context.xml"})
 @Transactional
+@Ignore
 public class GradeDaoTest {
 	Logger log = LoggerFactory.getLogger(GradeDaoTest.class);
 
