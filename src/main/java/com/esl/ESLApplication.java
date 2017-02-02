7becd0c7a4f7c059a352081a45d7c58520b10800
Timmy Wong <thcathy@gmail.com>
2017-02-02 10:52:11 +0800

    update to run in docker

--- src/main/java/com/esl/ESLApplication.java
+++ src/main/java/com/esl/ESLApplication.java
@@ -7,35 +7,16 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 import javax.servlet.DispatcherType;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import java.util.EnumSet;
 
 @SpringBootApplication
 @ImportResource("classpath:com/esl/ESL-context.xml")
+@PropertySource("classpath:auth0.properties")
 public class ESLApplication {
-
-    public void onStartup(ServletContext servletContext)
-            throws ServletException {
-
-        servletContext.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".xhtml");
-        servletContext.setInitParameter("javax.faces.PARTIAL_STATE_SAVING_METHOD", "true");
-        servletContext.setInitParameter("javax.faces.FACELETS_REFRESH_PERIOD", "1");
-        //servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-        servletContext.setInitParameter("javax.servlet.jsp.jstl.fmt.localizationContext", "resources.application");
-        //servletContext.setInitParameter("javax.faces.FACELETS_LIBRARIES", "/META-INF/resources/WEB-INF/taglib/esl.taglib.xml");
-    }
-
-    /*@Bean
-    public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet);
-        servletRegistrationBean.addUrlMappings("*.jsf","*.xhtml");
-        return servletRegistrationBean;
-    }*/
 
     @Bean
     public FilterRegistrationBean rewriteFilter() {
